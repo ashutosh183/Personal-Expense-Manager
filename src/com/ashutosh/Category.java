0d0e54b69670bf38d97e1d1f271d7bf3d45446d6
Ashutosh <ashutoshkumarpandey183@gmail.com>
2022-01-31 21:16:38 +0530

    Started Personal expense manager project

---
+++ src/com/ashutosh/Category.java
@@ -0,0 +1,16 @@
+package com.ashutosh;
+
+public class Category {
+	private String categoryType;
+	
+	public Category(String categoryType) {
+		this.categoryType = categoryType;
+	}
+	
+	public String getCategoryType() {
+		return categoryType;
+	}
+	public void setCategoryType(String categoryType) {
+		this.categoryType = categoryType;
+	}
+}
---
+++ src/com/ashutosh/Expense.java
@@ -0,0 +1,5 @@
+package com.ashutosh;
+
+public class Expense {
+
+}
